a267c8fc58bfa6e87d172cc7b13232a74415dc9f
Gustavo <alexgaria1975@gmail.com>
2025-06-04 23:15:11 -0500

    Actualizado renderer.js con nuevas credenciales de Google Cloud

--- renderer.js
+++ renderer.js
@@ -3,8 +3,8 @@ document.addEventListener('DOMContentLoaded', () => {
   const { utils, write, WorkBook } = XLSX;
 
   // Google Drive API configuration
-  const CLIENT_ID = 'YOUR_CLIENT_ID'; // Replace with your Google Cloud OAuth Client ID
-  const API_KEY = 'YOUR_API_KEY'; // Replace with your Google Cloud API Key
+  const CLIENT_ID = '429861903088-3h4275qhqb8pqvoi7e0th3e2n3k4ahbq.apps.googleusercontent.com'; // Replace with your Google Cloud OAuth Client ID
+  const API_KEY = 'AIzaSyA8QVoI7TQhp3oQKJlz2qCBKMNBErjnVPE'; // Replace with your Google Cloud API Key
   const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"];
   const SCOPES = 'https://www.googleapis.com/auth/drive.file';
   let gapiLoaded = false;
