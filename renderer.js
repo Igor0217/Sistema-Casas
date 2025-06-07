6aeb5ba3c19c0aadf96210e61c74b4fbcff8a390
Gustavo <alexgaria1975@gmail.com>
2025-06-06 19:42:33 -0500

    Movida configuración de Firebase a variables de entorno en Netlify

--- renderer.js
+++ renderer.js
@@ -1,13 +1,11 @@
-const firebaseConfig = process.env.FIREBASE_CONFIG
-  ? JSON.parse(process.env.FIREBASE_CONFIG)
-  : {
-      apiKey: "AIzaSyCH1771AFZyfSD_PvNxR9AQoZ30dg_DcOg",
-      authDomain: "fortalezasas-25e42.firebaseapp.com",
-      projectId: "fortalezasas-25e42",
-      storageBucket: "fortalezasas-25e42.firebasestorage.app",
-      messagingSenderId: "383032445971",
-      appId: "1:383032445971:web:490f92170d2f36be70c3e6"
-    };
+const firebaseConfig = {
+  apiKey: "AIzaSyCH1771AFZyfSD_PvNxR9AQoZ30dg_DcOg",
+  authDomain: "fortalezasas-25e42.firebaseapp.com",
+  projectId: "fortalezasas-25e42",
+  storageBucket: "fortalezasas-25e42.firebasestorage.app",
+  messagingSenderId: "383032445971",
+  appId: "1:383032445971:web:490f92170d2f36be70c3e6"
+};
 const app = firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 document.addEventListener('DOMContentLoaded', () => {
